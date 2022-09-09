1b004bdff5c5c7cc0dfa4a339fae2231f20526b0
Heng Ma <maheng1991@hotmail.com>
2022-09-09 15:22:44 -0700

    added disulfide bond entry creation

--- comp_sim/namd.py
+++ comp_sim/namd.py
@@ -4,8 +4,10 @@ import subprocess
 # import tempfile
 import numpy as np
 import MDAnalysis as mda
-from pydantic import BaseModel
 from pathlib import Path
+from pydantic import BaseModel
+
+from MDAnalysis.analysis import distances
 
 from .utils import build_logger
 
@@ -22,14 +24,16 @@ class NAMD_param(object):
     def __init__(
             self, pdb,
             add_sol=True,
+            disu_cutoff:float=3,
             ):
 
         self.pdb = pdb
-        self.pdb_label = pdb[:-4]
+        self.pdb_label = os.path.basename(pdb)[:-4]
         self.add_sol = add_sol
-        self.pdb_dissect()
+        self.disu_cutoff =disu_cutoff
+        self.build_segs()
 
-    def pdb_dissect(self):
+    def build_segs(self):
         """
         function to separate pdb file into each component of
         the complex
@@ -45,19 +49,26 @@ class NAMD_param(object):
             self.segments.append(
                 protein_seg(name=seg.segid, pdb=seg_save)
             )
+        
+        sulfur = mda_traj.select_atoms("resname CYS and name S*")
+        n_sulfur = sulfur.n_atoms
+        dist_disu = distances.self_distance_array(
+                sulfur.atoms.positions, box=sulfur.dimensions,
+            )
+        pairs = [[i, j] for i in range(n_sulfur) for j in range(i+1, n_sulfur)]
+        disu_pairs = np.array(pairs)[dist_disu < self.disu_cutoff]
+        self.disu_strs = []
+        for pair in disu_pairs: 
+            disu_st = [f"{sulfur[i].segid}:{sulfur[i].resid}" for i in pair]
+            self.disu_strs.append(' '.join(disu_st))
 
     def param_comp(self):
         """
         parameterize the complex
         """
-        comp_info = self.write_tleapIN()
-        subprocess.check_output(f'tleap -f leap.in', shell=True)
-        if (os.path.exists(comp_info['top_file']) and
-            os.path.exists(comp_info['pdb_file'])):
-            return comp_info
-        else:
-            raise Exception("Leap failed to build topology, check errors...")
-
+        self.write_psfgen()
+        subprocess.check_output(f'vmd -dispdev text -e psfgen.tcl', shell=True)
+        
     def write_psfgen(self):
         """
         produce tleap input file
@@ -71,11 +82,15 @@ class NAMD_param(object):
         template = jj_env.get_template("psfgen.j2")
         scripts = template.render(
             {'sys_label': self.pdb_label, 
-            'segs': self.segments}
+            'segs': self.segments, 
+            'disu_pairs': self.disu_strs
+            }
             )
         with open('psfgen.tcl', 'w') as f: 
             f.write(scripts)
 
+    def build_glyco(self): 
+        pass
 
         
 class protein_seg(BaseModel): 
