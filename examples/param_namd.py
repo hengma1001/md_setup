cd2579cd5f0186f4eafa8d5fda96ddecc1ec3a76
Heng Ma <maheng1991@hotmail.com>
2022-09-22 11:31:24 -0700

    changed pkg name

--- examples/param_namd.py
+++ examples/param_namd.py
@@ -1,6 +1,5 @@
-import sys
-sys.path.append("..") 
-
+import os, sys
+sys.path.append(os.path.abspath('..'))
 from comp_sim.param import NAMD_param
 
 pdb = './spike_wt_open_prot_gl/fragments_pdb/AS1.pdb'
