2925d41b6861a9332849471056db451ce88a21ad
Heng Ma <maheng1991@hotmail.com>
2022-09-22 11:20:28 -0700

    added namd glyco process

--- comp_sim/namd.py
+++ comp_sim/namd.py
@@ -13,6 +13,14 @@ from .utils import build_logger
 
 logger = build_logger()
 
+class protein_seg(BaseModel): 
+    name: str
+    pdb: Path
+
+class glyco_site(BaseModel): 
+    name: str
+    site: str
+
 
 class NAMD_param(object):
     """
@@ -25,42 +33,15 @@ class NAMD_param(object):
             self, pdb,
             add_sol=True,
             disu_cutoff:float=3,
+            glycosylation:bool=False,
             ):
 
         self.pdb = pdb
         self.pdb_label = os.path.basename(pdb)[:-4]
         self.add_sol = add_sol
-        self.disu_cutoff =disu_cutoff
+        self.disu_cutoff = disu_cutoff
+        self.glycosylation = glycosylation
         self.build_segs()
-
-    def build_segs(self):
-        """
-        function to separate pdb file into each component of
-        the complex
-
-        NOTE: it requires proper chain IDs for each segment
-        """
-        mda_traj = mda.Universe(self.pdb)
-        self.segments = []
-        # loop over segments in the complex
-        for seg in mda_traj.atoms.segments:
-            seg_save = f'{self.pdb_label}_seg{seg.segid}.pdb'
-            seg.atoms.write(seg_save)
-            self.segments.append(
-                protein_seg(name=seg.segid, pdb=seg_save)
-            )
-        
-        sulfur = mda_traj.select_atoms("resname CYS and name S*")
-        n_sulfur = sulfur.n_atoms
-        dist_disu = distances.self_distance_array(
-                sulfur.atoms.positions, box=sulfur.dimensions,
-            )
-        pairs = [[i, j] for i in range(n_sulfur) for j in range(i+1, n_sulfur)]
-        disu_pairs = np.array(pairs)[dist_disu < self.disu_cutoff]
-        self.disu_strs = []
-        for pair in disu_pairs: 
-            disu_st = [f"{sulfur[i].segid}:{sulfur[i].resid}" for i in pair]
-            self.disu_strs.append(' '.join(disu_st))
 
     def param_comp(self):
         """
@@ -83,16 +64,66 @@ class NAMD_param(object):
         scripts = template.render(
             {'sys_label': self.pdb_label, 
             'segs': self.segments, 
-            'disu_pairs': self.disu_strs
+            'disu_pairs': self.disu_strs,
+            'glyco_sites': self.glyco_sites
             }
             )
         with open('psfgen.tcl', 'w') as f: 
             f.write(scripts)
 
-    def build_glyco(self): 
-        pass
+    def build_segs(self):
+        """
+        function to separate pdb file into each component of
+        the complex
+
+        NOTE: it requires proper chain IDs for each segment
+        """
+        mda_u = mda.Universe(self.pdb)
+        self.segments = []
+        # loop over segments in the complex
+        for seg in mda_u.atoms.segments:
+            seg_save = f'{self.pdb_label}_seg{seg.segid}.pdb'
+            seg.atoms.write(seg_save)
+            self.segments.append(
+                protein_seg(name=seg.segid, pdb=seg_save)
+            )
+        self._build_disu(mda_u)
+        if self.glycosylation == True:
+            self._build_glyco(mda_u)
+        else: 
+            self.glyco_sites = []
+
+    
+    def _build_disu(self, mda_u):
+        sulfur = mda_u.select_atoms("resname CYS and name S*")
+        n_sulfur = sulfur.n_atoms
+        dist_disu = distances.self_distance_array(
+                sulfur.atoms.positions, box=sulfur.dimensions,
+            )
+        pairs = [[i, j] for i in range(n_sulfur) for j in range(i+1, n_sulfur)]
+        disu_pairs = np.array(pairs)[dist_disu < self.disu_cutoff]
+        self.disu_strs = []
+        for pair in disu_pairs: 
+            disu_st = [f"{sulfur[i].segid}:{sulfur[i].resid}" for i in pair]
+            self.disu_strs.append(' '.join(disu_st))
+
+    def _build_glyco(self, mda_u):
+        """
+        Find and add glyco to the N-glycosylation site on proteins if 
+        required, Asn-X-Ser/Thr, X is any AA other than Pro. It's only 
+        designed for the spike RBD sites for the moment as limited by 
+        glyco type. 
+        """ 
+        gly_sites = [res for res in mda_u.residues 
+                if res.resname == 'ASN' 
+                and mda_u.residues.resnames[res.resindex+1] != 'PRO' 
+                and mda_u.residues.resnames[res.resindex+2] in ['SER', 'THR']]
+        
+        glyco_sites = []
+        for i, res in enumerate(gly_sites):
+            site = f"{res.segid}:{res.resid}"
+            glyco_sites.append(glyco_site(name=f"g{i}", site=site))
+        self.glyco_sites = glyco_sites
+        logger.debug(self.glyco_sites)
 
         
-class protein_seg(BaseModel): 
-    name: str
-    pdb: Path
---
+++ examples/param_namd.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("..") 
+
+from comp_sim.param import NAMD_param
+
+pdb = './spike_wt_open_prot_gl/fragments_pdb/AS1.pdb'
+namd = NAMD_param(pdb, glycosylation=True) 
+namd.param_comp()
