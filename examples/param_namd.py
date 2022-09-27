97a048865001f0e588f3bf0a2b4777030676c9a1
Heng Ma <maheng1991@hotmail.com>
2022-09-27 12:17:37 -0700

    added chain selection to glycosylation

--- examples/param_namd.py
+++ examples/param_namd.py
@@ -2,6 +2,6 @@ import os, sys
 sys.path.append("..")
 from md_setup.param import NAMD_param
 
-pdb = './spike_wt_open_prot_gl/fragments_pdb/AS1.pdb'
-namd = NAMD_param(pdb, glycosylation=True) 
+pdb = "pdbs/7zdq.pdb" # './spike_wt_open_prot_gl/fragments_pdb/AS1.pdb'
+namd = NAMD_param(pdb, glycosylation='B') 
 namd.param_comp()
