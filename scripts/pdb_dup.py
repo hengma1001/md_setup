5367fa2b6bde2e49b9c43b73d532edfe9ee6d2c8
Heng Ma <maheng1991@hotmail.com>
2022-10-17 15:21:22 -0700

    added max copy limits

--- scripts/pdb_dup.py
+++ scripts/pdb_dup.py
@@ -4,24 +4,25 @@ from openmm.app.modeller import Modeller
 from itertools import product
 from openmm import unit
 
-pdb_file = "../../pdbs/ncd_dimers_0.pdb"
+pdb_file = "ncd_dimers_0.pdb"
 pdb = PDBFile(pdb_file)
 label = os.path.basename(pdb_file)[:-4]
 n = 2
+n_copy_max = 4 # n**3
 spacing = 10.0
 output_filename = f"{label}_{n}_{int(spacing)}nm.pdb"
 
 model = Modeller(pdb.topology, pdb.positions)
-model.deleteWater()
-model.delete([res for res in model.topology.residues() if "+" in res.name])
-model.delete([res for res in model.topology.residues() if "-" in res.name])
-model.delete([chain for chain in model.topology.chains() if chain.index > 1])
+# model.deleteWater()
+# model.delete([res for res in model.topology.residues() if "+" in res.name])
+# model.delete([res for res in model.topology.residues() if "-" in res.name])
+# model.delete([chain for chain in model.topology.chains() if chain.index > 1])
 
 topology_0 = model.topology
 position_lists = [model.getPositions()]
 # --- For n in N ---
 
-protein_coordinates = list(product(list(range(n)),list(range(n)),list(range(n))))[1:] #cubic n x n x n array of coordinates
+protein_coordinates = list(product(list(range(n)),list(range(n)),list(range(n))))[1:n_copy_max] #cubic n x n x n array of coordinates
 for i, delta_xyz in enumerate(protein_coordinates):
     p_0 = position_lists[(i+1) % len(position_lists) ]
     d_p = [di* spacing for di in delta_xyz] * unit.nanometer
