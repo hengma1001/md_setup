dd35c2079de5da3f51611039b76326e7d8d6dd61
Heng Ma <maheng1991@hotmail.com>
2022-10-17 15:32:02 -0700

    added new to gitignore and updated amber

--- md_setup/amber.py
+++ md_setup/amber.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 # import tempfile
-import numpy as np
 import MDAnalysis as mda
 
 from .utils import add_hydrogen, remove_hydrogen, missing_hydrogen
@@ -9,7 +8,6 @@ from .utils import build_logger
 from .utils import match_pdb_to_amberff
 
 logger = build_logger()
-
 
 class AMBER_param(object):
     """
@@ -22,12 +20,33 @@ class AMBER_param(object):
             self, pdb,
             add_sol=True,
             lig_charge=0,
-            keep_H=False):
+            keep_H=False, 
+            forcefield='ff19SB', 
+            forcefield_rna='OL3',
+            forcefield_dna='OL15',
+            watermodel='opc',
+            padding=20, 
+            cubic=True, 
+            cation='Na+', 
+            n_cations=0, 
+            anion='Cl-', 
+            n_anions='0'):
 
         self.pdb = pdb
+        self.label = os.path.basename(pdb)[:-4]
         self.add_sol = add_sol
         self.lig_charge = lig_charge
         self.keep_H = keep_H
+        self.forcefield = forcefield
+        self.forcefield_rna = forcefield_rna
+        self.forcefield_dna = forcefield_dna
+        self.watermodel = watermodel
+        self.padding = padding
+        self.cubic = cubic
+        self.cation = cation
+        self.n_cations = n_cations
+        self.anion = anion 
+        self.n_anions = n_anions
         self.pdb_dissect()
 
     def pdb_dissect(self):
@@ -40,14 +59,21 @@ class AMBER_param(object):
         mda_traj = mda.Universe(self.pdb)
         self.prot_files = []
         self.lig_files = []
+        # not_na_not_protein = mda_traj.select_atoms('not protein and not nucleic')
+        # if not_na_not_protein.n_atoms == 0: 
+        #     self.prot_files.append(self.pdb)
+        #     return
+
         # loop over segments in the complex
         for seg in mda_traj.atoms.segments:
             protein = seg.atoms.select_atoms('protein')
             not_protein = seg.atoms.select_atoms('not protein')
-            logger.info(protein.n_atoms, not_protein.n_atoms)
+            na_not_protein = not_protein.atoms.select_atoms('nucleic')
+            not_na_not_protein = not_protein.atoms.select_atoms('not nucleic')
+            print(protein.n_atoms, not_protein.n_atoms)
             # processing protein
             if protein.n_atoms != 0:
-                protein_save = f'prot_seg{seg.segid}.pdb'
+                protein_save = f'prot_seg{seg.segid}_{seg.segindex}.pdb'
                 protein.write(protein_save)
                 if self.keep_H and not missing_hydrogen(protein_save):
                     match_pdb_to_amberff(protein_save)
@@ -55,6 +81,12 @@ class AMBER_param(object):
                     remove_hydrogen(protein_save, protein_save)
                 self.prot_files.append(protein_save)
 
+            # processing na
+            if na_not_protein.n_atoms != 0: 
+                na_not_protein_save = f'na_seg{seg.segid}.pdb'
+                na_not_protein.wrote(na_not_protein_save) 
+                self.prot_files.append(na_not_protein_save)
+            
             # processing ligands
             if not_protein.n_atoms != 0:
                 for i, res in enumerate(not_protein.residues):
@@ -107,20 +139,23 @@ class AMBER_param(object):
         produce tleap input file
         """
         output_path = os.path.abspath(os.path.dirname(self.pdb))
-        output_top = os.path.join(output_path, 'comp.prmtop')
-        output_inpcrd = os.path.join(output_path, 'comp.inpcrd')
-        output_pdb = os.path.join(output_path, 'comp.pdb')
+        output_top = os.path.join(output_path, f'{self.label}.prmtop')
+        output_inpcrd = os.path.join(output_path, f'{self.label}.inpcrd')
+        output_pdb = os.path.join(output_path, f'{self.label}.pdb')
         with open(f'leap.in', 'w+') as leap:
             # default protein ff
-            leap.write("source leaprc.protein.ff14SB\n")
+            leap.write(f"source leaprc.protein.{self.forcefield}\n")
+            leap.write(f"source leaprc.RNA.{self.forcefield_rna}\n")
+            leap.write(f"source leaprc.DNA.{self.forcefield_dna}\n")
             leap.write("source leaprc.gaff\n")
-            leap.write("source leaprc.water.tip3p\n")
+            leap.write(f"source leaprc.water.{self.watermodel}\n")
+            # leap.write(f"source leaprc.")
             leap.write("set default PBRadii mbondi3\n")
 
             # load proteins
             prot_insts = []
             for i, prot in enumerate(self.prot_files):
-                leap_name = f'rec{i}'
+                leap_name = f'biom{i}'
                 leap.write(f"{leap_name} = loadPDB {prot}\n")
                 prot_insts.append(leap_name)
             prot_insts = ' '.join(prot_insts)
@@ -139,9 +174,13 @@ class AMBER_param(object):
             combine_insts = prot_insts + ' ' + lig_insts
             leap.write(f"comp = combine {{ {combine_insts} }}\n")
             if self.add_sol:
-                leap.write("solvatebox comp TIP3PBOX 15\n")
-                leap.write("addions comp Na+ 0\n")
-                leap.write("addions comp Cl- 0\n")
+                leap.write(f"solvatebox comp {self.watermodel.upper()}BOX {self.padding}")
+                if self.cubic: 
+                    leap.write(f" iso\n")
+                else: 
+                    leap.write('/n')
+                leap.write(f"addions comp {self.cation} {self.n_cations}\n")
+                leap.write(f"addions comp {self.anion} {self.n_anions}\n")
             leap.write(f"saveAmberParm comp {output_top} {output_inpcrd}\n")
             leap.write(f"savepdb comp {output_pdb}\n")
             leap.write("quit\n")
